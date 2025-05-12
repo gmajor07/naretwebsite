040632976c301d1fe4d705d26cd8669e2c697617
gMajor07 <gkyando5@gmail.com>
2025-05-12 14:00:44 +0300

    update the whole webiste

--- header.php
+++ header.php
@@ -66,12 +66,13 @@ $clients = getClients();
         <!-- Navbar Start -->
         <div class="container-fluid nav-bar bg-transparent">
             <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
-                <a href="index.php" class="navbar-brand d-flex align-items-center text-center">
-                    <div class="icon p-2 me-2">
-                        <img class="img-fluid" src="img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
-                    </div>
-                    <h1 class="m-0 text-primary">NARET COMPANY LIMITED</h1>
-                </a>
+               <a href="index.php" class="navbar-brand d-flex align-items-center text-center flex-wrap">
+    <div class="icon p-2 me-2">
+        <img class="img-fluid" src="img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
+    </div>
+    <span class="text-primary fw-bold" style="font-size: 2rem;">NARET COMPANY LIMITED</span>
+</a>
+
                 <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
