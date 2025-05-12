369dddb2ff6e9747e4f958556136b9e32f0708c2
gMajor07 <gkyando5@gmail.com>
2025-05-12 14:05:42 +0300

    update the whole webiste

--- header.php
+++ header.php
@@ -66,17 +66,21 @@ $clients = getClients();
         <!-- Navbar Start -->
         <div class="container-fluid nav-bar bg-transparent">
             <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
-               <a href="index.php" class="navbar-brand d-flex align-items-center text-center flex-wrap">
-    <div class="icon p-2 me-2">
-        <img class="img-fluid" src="img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
-    </div>
-    <span class="text-primary fw-bold" style="font-size: 2rem;">NARET COMPANY LIMITED</span>
-</a>
+       <div class="d-flex flex-column flex-sm-row align-items-center justify-content-between w-100">
+    <!-- Brand (logo + name) -->
+    <a href="index.php" class="navbar-brand d-flex align-items-center text-center mb-2 mb-sm-0">
+        <div class="icon p-2 me-2">
+            <img class="img-fluid" src="img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
+        </div>
+        <span class="text-primary fw-bold" style="font-size: 1.5rem;">NARET COMPANY LIMITED</span>
+    </a>
 
-<br>
-                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <!-- Toggle Button -->
+    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</div>
+
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto">
                         <a href="index.php" class="nav-item nav-link active">Home</a>
